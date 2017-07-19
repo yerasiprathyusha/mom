60a6ed0edc90604368d6b8ec9f21cf2494079e05
Prathyusha Yerasi <y.prathyusha@globaledgesoft.com>
2017-07-19 17:24:44 +0530

    make it better

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/api/mom', (req, res) => {
-  console.log(req.body.context);
+  console.log(req.body.context.System);
   //res.send(req.body);
   if(req.body.request.intent.name == 'StartIntent'){
 	knex('meetinginfo').insert({start_time:Date.now()})
